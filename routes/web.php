35c40ba16aa2a74326e210edb8b508d4b1db9949
nonono4634 <yoohyooh4634@gmail.com>
2022-11-06 02:48:51 +0900

    Item

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ItemController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('items', ItemController::Class) 
+->middleware(['auth', 'verified']);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
